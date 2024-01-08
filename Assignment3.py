76b169c984fe260947ec393fbc1b89e8b064fb20
EazyEzz <essmer.sanchez99@qmail.cuny.edu>
2024-01-08 13:48:58 -0500

    Completed Assignment3 .py, .html, and mystyle.css

--- Assignment3.py
+++ Assignment3.py
@@ -7,23 +7,33 @@
 
 import OutputUtil as OU
 
-# [0] Perform the usual preliminary tasks as outlined in Assignment #1
+
 # [1] Define a function to read in the United States data from file "us-states.csv" into a two-dimensional list..
 def read_file(fileName):
     with open(fileName) as file:
         lines = file.readlines()
         states = [line.strip().split(',') for line in lines]
-        return states[0],states[1:]
+        return states[0], states[1:]
         # print(states)
 
 
 def main():
-    title = "US States"
+    title = "Essmer Sanchez's US States Page"
     alignments = ["l", "l", "l", "r"]
     types = ["S", "S", "S", "N"]
-    outputFile = "Assignment03.html"
+    output_file = "Assignment3.html"
     headers, states = read_file('States.csv')
-    OU.write_html_file(outputFile, title, headers, types, alignments, states, True)
+    for i in range(len(states)):
+        name = states[i][0]
+        if name == "New York":
+            wikiname = "New_York_(state)"
+        else:
+            wikiname = name
+        href = "https://en.wikipedia.org/wiki/" + wikiname.replace(' ', '_')
+        a_attributes = 'href="' + href + '" target="_blank"'
+        # td_cont = OU.create_element(OU.TAG_A, states[i][0], a_attributes)
+        states[i][0] = OU.create_element(OU.TAG_A, name, a_attributes)
+    OU.write_html_file(output_file, title, headers, types, alignments, states, True)
 
     # read_file('States.csv')
 
